0e9d828584a0cffb40c484c212ea4c8defa3feba
Erlangga <erlangga@cactiva.app>
2020-08-09 22:58:37 +0700

    fix

--- src/Pages/Landing/MenuBar.jsx
+++ src/Pages/Landing/MenuBar.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { observer, useObservable } from "mobx-react-lite";
-import { Button, AppBar, Typography } from "@material-ui/core";
+import {
+  Button,
+  AppBar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
 import Logo from "./logo.png";
+import { useMediaQuery } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 export default observer(() => {
-  const meta = useObservable({
-    isCallVisible: false,
-  });
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const style = {
     appBar: {
@@ -25,23 +40,71 @@ export default observer(() => {
       display: "flex",
       padding: 10,
     },
+    a: {
+      textDecoration: "none",
+    },
   };
+
+  // const isDesktop = useMediaQuery("(min-width:1224px)");
+
+  const isMobile = useMediaQuery("(max-width: 1224px)");
 
   return (
     <>
       <AppBar style={style.appBar}>
         <Button style={style.icon} href={"#beranda"}>
           <Typography style={style.icon.text} variant="h6">
-            <img src={Logo} style={{width :51, height:51}}/>
+            <img src={Logo} style={{ width: 51, height: 51 }} />
           </Typography>
         </Button>
         <div style={style.menu}>
-          <Button href={"#beranda"}>Beranda</Button>
-          <Button href={"#tentang-kami"}>Tentang Kami</Button>
-          <Button href={"#kepengurusan"}>Kepengurusan</Button>
-          <Button href={"#kontak"}>Kontak</Button>
+          {isMobile ? (
+            <IconButton
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              {console.log(anchorEl)}
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <>
+              <Button href={"#beranda"}>Beranda</Button>
+              <Button href={"#tentang-kami"}>Tentang Kami</Button>
+              <Button href={"#kepengurusan"}>Kepengurusan</Button>
+              <Button href={"#kontak"}>Kontak</Button>
+            </>
+          )}
         </div>
       </AppBar>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <a style={style.a} href="#beranda">
+            Beranda
+          </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <a style={style.a} href="#tentang-kami">
+            Tentang Kami
+          </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <a style={style.a} href="#kepengurusan">
+            Kepengurusan
+          </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <a style={style.a} href="#kontak">
+            Kontak
+          </a>
+        </MenuItem>
+      </Menu>
     </>
   );
 });
