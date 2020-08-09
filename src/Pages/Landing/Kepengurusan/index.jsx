0487e0bdeb86d870360bc4300cb3b7f98eb78788
Elang-Eka <elangputra87@gmail.com>
2020-08-09 08:29:22 +0700

    kepengurusan_update

--- src/Pages/Landing/Kepengurusan/index.jsx
+++ src/Pages/Landing/Kepengurusan/index.jsx
@@ -1,10 +1,204 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
+import "./w3.css";
 
 export default observer(() => {
   return (
     <div id="kepengurusan" style={{top:"20px"}}>
-        
+    <div class="w3-card-1" class="w3-center"><h1>Kepengurusan Ju-Jitsu UPN "Veteran" Jatim (Honey Badger)</h1></div>
+    <div style={{display:"flex",flexDirection:"row",justifyContent:"center"}}>
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Ketua Umum Honey Badger</h3></th>
+            <tr>
+              <th class="w3-left-align">Nama</th>
+              <th>:</th>
+              <td>Mohammad Akbar Hakiki</td>
+            </tr>
+            <tr>
+              <th class="w3-left-align">Fakultas</th>
+              <th>:</th>
+              <td>Fakultas Ekonomi & Bisnis</td>
+            </tr>
+            <tr>
+              <th class="w3-left-align">Prodi</th>
+              <th>:</th>
+              <td>Akuntansi</td>
+            </tr>
+            <tr>
+              <th class="w3-left-align"class="w3-left-align">NPM</th>
+              <th>:</th>
+              <td>17013010100</td>
+            </tr>
+        </table>
+
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Wakil Ketua Umum Honey Badger</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>Muhammad Nashir Hasan</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Fakultas</th>
+              <th>:</th>
+              <td>Fakultas Ekonomi & Bisnis</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Prodi</th>
+              <th>:</th>
+              <td>Manajemen</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>NPM</th>
+              <th>:</th>
+              <td>1912010183</td>
+            </tr>
+        </table>
+        </div>
+
+        <div style={{display:"flex",flexDirection:"row",justifyContent:"center"}}>
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Sektretaris 1</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>Niko Kurniawan</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Fakultas</th>
+              <th>:</th>
+              <td>Fakultas Ilmu Sosial & Ilmu Politik</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Prodi</th>
+              <th>:</th>
+              <td>Ilmu Komunikasi</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>NPM</th>
+              <th>:</th>
+              <td>18043010008</td>
+            </tr>
+        </table>
+
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Sekretaris 2</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>Nur Fajri Nishfi Sya'baniyah</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Fakultas</th>
+              <th>:</th>
+              <td>Fakultas Teknik</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Prodi</th>
+              <th>:</th>
+              <td>Teknik Lingkungan</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>NPM</th>
+              <th>:</th>
+              <td>18034010029</td>
+            </tr>
+        </table>
+        </div>
+
+        <div style={{display:"flex",flexDirection:"row",justifyContent:"center"}}>
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Bendahara 1</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>Bisma Bayu Putra</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Fakultas</th>
+              <th>:</th>
+              <td>Fakultas Pertanian</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Prodi</th>
+              <th>:</th>
+              <td>Agribisnis</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>NPM</th>
+              <th>:</th>
+              <td>19024010105</td>
+            </tr>
+        </table>
+
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Bendahara 2</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>@Adiesabta</td>
+            </tr>
+        </table>
+        </div>
+
+        <div style={{display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Div Pengembangan atlet</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>Adelya Eka Cahyono Putri</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Fakultas</th>
+              <th>:</th>
+              <td>Fakultas Ekonomi & Bisnis</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Prodi</th>
+              <th>:</th>
+              <td>Akuntansi</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>NPM</th>
+              <th>:</th>
+              <td>19013010062</td>
+            </tr>
+        </table>
+
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Div Humas</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>@Alifio</td>
+            </tr>
+        </table>
+
+        <table style={{padding:"20px",borderRadius:"5px",marginTop:"20px"}} class="w3-card-4">
+            <th><h3>Div Perlengkapan</h3></th>
+            <tr class="w3-left-align">
+              <th>Nama</th>
+              <th>:</th>
+              <td>Jefri Abdurrozak Ismail</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Fakultas</th>
+              <th>:</th>
+              <td>Fakultas Ilmu Komputer</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>Prodi</th>
+              <th>:</th>
+              <td>Informatika</td>
+            </tr>
+            <tr class="w3-left-align">
+              <th>NPM</th>
+              <th>:</th>
+              <td>18081010059</td>
+            </tr>
+        </table>
+        </div>
     </div>
   )
   
