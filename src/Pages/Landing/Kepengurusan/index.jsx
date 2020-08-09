1da4853c32bd287d2c97838dc4159b088c9c732b
JuukyoKai <juukyokai00@gmail.com>
2020-08-09 19:56:20 +0700

    pull

--- src/Pages/Landing/Kepengurusan/index.jsx
+++ src/Pages/Landing/Kepengurusan/index.jsx
@@ -5,8 +5,8 @@ import Prototype from './prototype.js';
 
 export default observer(() => {
   return (
-    <div id="kepengurusan" style={{top:"20px"}}>
-      <Prototype />
+    <div id="kepengurusan" style={{top:"20px"}} data-aos="fade">
+      <Prototype data-aos-delay="200"/>
     </div>
   )
   
