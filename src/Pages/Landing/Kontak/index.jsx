6f0ab49acf0a360408121c1d7c87ccc52a8b390c
JuukyoKai <juukyokai00@gmail.com>
2020-08-09 12:40:27 +0700

    prototipe kepengurusan

--- src/Pages/Landing/Kontak/index.jsx
+++ src/Pages/Landing/Kontak/index.jsx
@@ -6,16 +6,12 @@ import "./w3.css";
 export default observer(() => {
   return (
     <div id="kontak">
-      <div class="mySlides w3-animate-opacity w3-content w3-display-container w3-black w3-center" style={{"top":"-175px", "left":"-100px", "z-index":"+1" }}>
-            <div class="w3-display-left w3-padding w3-hide-small" style={{width:"35%"}}>
-            <div class="w3-black w3-hover-opacity-off w3-padding-large w3-round-large">
-            <InstagramEmbed url="https://www.instagram.com/p/B6ewClOFUOb/" hideCaption={true}/>
-            </div>
-            </div>
-        </div>
+      
       
       
       <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-xlarge">
+      
+      
       <div class="w3-section">
         <i class="fa fa-facebook-official w3-hover-opacity" style={{padding:"5px"}}> </i>
         <a href="https://www.instagram.com/jiujitsu_upn/"><i class="fa fa-instagram w3-hover-opacity" style={{padding:"5px"}}></i></a>
