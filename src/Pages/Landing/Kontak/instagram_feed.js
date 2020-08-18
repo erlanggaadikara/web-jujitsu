1f5307a07b6b8abb908d60acff3c172a50f05aee
JuukyoKai <juukyokai00@gmail.com>
2020-08-18 20:37:12 +0700

    responsive instagram fix

--- src/Pages/Landing/Kontak/instagram_feed.js
+++ src/Pages/Landing/Kontak/instagram_feed.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import InstagramEmbed from 'react-instagram-embed';
+import Slider from './instagram_slider';
 import "./w3.css";
 
 export default function instagram_feed(){
     return(
-        <div>  
+        <div>
+            
             <h1 class="w3-xxxlarge"><b>Social Media Activities</b></h1>
             <p class="w3-center" style={{"font-size":"20px"}}>What we do on the internet</p>
-            <div style={{display:"flex",flexDirection:"row",justifyContent:"center"}}>
-                <InstagramEmbed url="https://www.instagram.com/p/B6ewClOFUOb/" hideCaption={true} style={{margin:"10px 10px"}}/>
-                <InstagramEmbed url="https://www.instagram.com/p/BaqPf9OBMGW/" hideCaption={true} style={{margin:"10px 10px"}}/>
-                <InstagramEmbed url="https://www.instagram.com/p/B5RmyKFlxOo/" hideCaption={true} style={{margin:"10px 10px"}}/>
+            <div class="w3-row-padding w3-hide-small" style={{display:"flex",flexDirection:"row",justifyContent:"center",width:"100%"}}>
+                <InstagramEmbed url="https://www.instagram.com/p/B6ewClOFUOb/" class="w3-card l4 m3" style={{width:"100%"}} hideCaption={true} style={{margin:"10px 10px"}}/>
+                <InstagramEmbed url="https://www.instagram.com/p/BaqPf9OBMGW/" class="w3-card l4 m3" style={{width:"100%"}} hideCaption={true} style={{margin:"10px 10px"}}/>
+                <InstagramEmbed url="https://www.instagram.com/p/B5RmyKFlxOo/" class="w3-card l4 m3 w3-hide-medium" style={{width:"100%"}} hideCaption={true} style={{margin:"10px 10px"}}/>
+            </div>
+            <div class="w3-row-padding w3-hide-large w3-hide-medium" style={{display:"flex",flexDirection:"row",justifyContent:"center",width:"100%"}}>
+                <Slider />
             </div>
         </div>
     )
---
+++ src/Pages/Landing/Kontak/instagram_slider.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import InstagramEmbed from 'react-instagram-embed';
+import './App.css';
+import './w3.css';
+
+export default class slideshow extends React.Component {
+  state = {
+    currentIndex: 0,
+    itemsInSlide: 1,
+    responsive: { 3: { items: 1 } },
+    galleryItems: [
+        <InstagramEmbed url="https://www.instagram.com/p/B6ewClOFUOb/" class="w3-card" style={{width:"100%"}} hideCaption={true} style={{margin:"10px 10px"}}/>,
+        <InstagramEmbed url="https://www.instagram.com/p/BaqPf9OBMGW/" class="w3-card" style={{width:"100%"}} hideCaption={true} style={{margin:"10px 10px"}}/>,
+        <InstagramEmbed url="https://www.instagram.com/p/B5RmyKFlxOo/" class="w3-card" style={{width:"100%"}} hideCaption={true} style={{margin:"10px 10px"}}/>
+            
+    ],
+  }
+ 
+  slidePrevPage = () => {
+    const currentIndex = this.state.currentIndex - this.state.itemsInSlide
+    this.setState({ currentIndex })
+  }
+ 
+  slideNextPage = () => {
+    const {
+      itemsInSlide,
+      galleryItems: { length },
+    } = this.state
+    let currentIndex = this.state.currentIndex + itemsInSlide
+    if (currentIndex > length) currentIndex = length
+ 
+    this.setState({ currentIndex })
+  }
+ 
+  handleOnSlideChange = (event) => {
+    const { itemsInSlide, item } = event
+    this.setState({ itemsInSlide, currentIndex: item })
+  }
+  
+  render() {
+    const { currentIndex, galleryItems, responsive } = this.state;
+      return (
+      <div style={{display:"flex",width:"480px",justifyContent:"center"}}>
+        <AliceCarousel
+          items={galleryItems}
+          autoPlay={true}
+          autoPlayInterval={3000}
+          fadeOutAnimation={true}
+          dotsDisabled={false}
+          buttonsDisabled={true}
+          mouseTrackingEnabled={false}
+          slideToIndex={currentIndex}
+          responsive={responsive}
+          onInitialized={this.handleOnSlideChange}
+          onSlideChanged={this.handleOnSlideChange}
+          onResized={this.handleOnSlideChange}
+        />
+      </div>
+    )
+  }
+}
