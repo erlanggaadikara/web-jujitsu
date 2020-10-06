66a35177324fecb8dee4fd96a9fc9720e3181c03
Erlangga <erlangga@cactiva.app>
2020-10-06 14:14:29 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import RouteMap from "./RouteMap";
 import Sidebar from "./Pages/Dashboard/Admin/Sidebar";
 
